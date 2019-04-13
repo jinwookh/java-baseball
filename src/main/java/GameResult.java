77cab68d78153ae6fc435280266f7d35c19ebfce
jinwook <jin942002@naver.com>
2019-04-13 23:12:35 +0900

    한 자리 수와 세 자리 수를 비교하여 strike, ball, miss 중 하나의 결과를 리턴하는 함수 구현.
    함수이름: isStrikeOrBallOrMiss() in GameResult class.

--- src/main/java/GameResult.java
+++ src/main/java/GameResult.java
@@ -1,24 +1,68 @@
+import java.util.*;
+
 public class GameResult {
-    private int strike;
-    private int ball;
+    private int STRIKE = 1;
+    private int BALL = 2;
+    private int MISS = 0;
+
+    private int strike = 0;
+    private int ball = 0;
 
     GameResult(GameNumber userNumber, GameNumber generatedNumber) {
-        countStrike(userNumber.toString(), generatedNumber.toString());
+        this(userNumber.toString(), generatedNumber.toString());
     }
 
-    private void countStrike(String userNumber, String generatedNumber) {
-        for(int i = 0; i < GameNumber.LENGTH; i++) {
-            if(isStrike(userNumber.charAt(i), generatedNumber, i)) {
-                strike++;
-            }
+    GameResult(String userNumber, String generatedNumber) {
+        for(int i = 0; i< GameNumber.LENGTH; i++) {
+            int result = isStrikeOrBallOrMiss(userNumber.charAt(i), generatedNumber, i);
+            adaptResult(result);
         }
     }
+
+    private int isStrikeOrBallOrMiss(char userCharacter,
+                                     String generatedNumber, int index) {
+        if(isStrike(userCharacter, generatedNumber, index)) {
+            return STRIKE;
+        }
+        if(isBall(userCharacter, generatedNumber, index)) {
+            return BALL;
+        }
+        return MISS;
+    }
+
+    private void adaptResult(int result) {
+        if(result == STRIKE) {
+            strike++;
+            return;
+        }
+        if(result == BALL) {
+            ball++;
+        }
+    }
+
 
     private boolean isStrike(char userCharacter, String generatedNumber, int index) {
         if(userCharacter == generatedNumber.charAt(index)) {
             return true;
         }
         return false;
+    }
+
+    private boolean isBall(char userCharacter, String generatedNumber, int index) {
+        HashSet<Character> generatedNumberSet
+                = new HashSet<Character>(stringtoCharacterList(generatedNumber));
+        if(generatedNumberSet.contains(Character.valueOf(userCharacter))) {
+            return true;
+        }
+        return false;
+    }
+
+    private List<Character> stringtoCharacterList(String string) {
+        List<Character> characterList = new ArrayList<>();
+        for(char character : string.toCharArray()) {
+            characterList.add(character);
+        }
+        return characterList;
     }
 
 
