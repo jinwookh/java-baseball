a908bdd2b9058a430118e95ef138497409a1445c
jinwook <jin942002@naver.com>
2019-04-13 22:04:44 +0900

    사용자에게 입력받는 선택지(1 혹은 2)에 대한 예외를 처리하는 함수 구현.
    함수이름: checkValidity() in Choice class.

---
+++ src/main/java/Choice.java
@@ -0,0 +1,23 @@
+public class Choice {
+    private static final String GO = "1";
+    private static final String STOP = "2";
+    private static final String WRONG_CHOICE_ERROR = "1 또는 2가 아닙니다.";
+    private String choice;
+
+    Choice(String input) {
+        if(!checkValidity(input)) {
+            throw new IllegalArgumentException(WRONG_CHOICE_ERROR);
+        }
+        choice = input;
+    }
+
+    private boolean checkValidity(String string) {
+        if(string.equals(GO) || string.equals(STOP)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
--- src/main/java/GameNumber.java
+++ src/main/java/GameNumber.java
@@ -2,13 +2,14 @@ public class GameNumber {
     private static int LENGTH = 3;
     private static char BOTTOM = '1';
     private static char TOP = '9';
+    private static String WRONG_NUMBER_FORMAT_ERROR = "1~9로 이루어진 세 자리 숫자를" +
+            " 입력해 주세요.";
 
     private String number;
 
     GameNumber(String input) {
         if(!checkValidity(input)) {
-            throw new IllegalArgumentException("1~9로 이루어진 세 자리 숫자를" +
-                    " 입력해 주세요.");
+            throw new IllegalArgumentException(WRONG_NUMBER_FORMAT_ERROR);
         }
         number = input;
     }
