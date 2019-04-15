106459c6bcc7f4012266b079b7628040c4fde472
jinwook <jin942002@naver.com>
2019-04-15 14:54:53 +0900

    하나의 수에 대해 볼인지 판별하는 기능 구현.
    함수 이름: isBall() in RoundResult enum.

--- src/main/java/RoundResult.java
+++ src/main/java/RoundResult.java
@@ -8,14 +8,24 @@ public enum RoundResult {
         if (isStrike(userCharacter, generatedNumber, index)) {
             return STRIKE;
         }
-
-
-        return RoundResult.BALL;
+        if (isBall(userCharacter, generatedNumber)) {
+            return BALL;
+        }
+        return MISS;
     }
 
     private boolean isStrike(char userCharacter, String generatedNumber, int index) {
         if (generatedNumber.charAt(index) == userCharacter) {
             return true;
+        }
+        return false;
+    }
+
+    private boolean isBall(char userCharacter, String generatedNumber) {
+        for (int i = 0; i < generatedNumber.length(); i++) {
+            if (generatedNumber.charAt(i) == userCharacter) {
+                return true;
+            }
         }
         return false;
     }
